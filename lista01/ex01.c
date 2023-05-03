f680813cc4ebe115e7a685dc1b05c91ab59b5d2d
Lucasvgb <lucasvgb@id.uff.br>
2023-05-02 21:07:26 -0300

    ex01

---
+++ lista01/ex01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+1. Fazer um programa para receber um número inteiro de segundos do usuário e imprimir
+a quantidade correspondente em horas, minutos e segundos.
+*/
+int main(){
+
+  int horas,minutos,segundos,resto;
+
+  printf("Digite os segundos: ", segundos);
+  scanf("%d",&segundos);
+
+  horas = segundos / 3600;
+  resto = segundos % 3600;
+  minutos = resto / 60;
+  segundos = resto % 60;
+  printf("%i horas, %i minutos, %i segundos\n", horas,minutos,segundos);
+
+  return 0;
+}
