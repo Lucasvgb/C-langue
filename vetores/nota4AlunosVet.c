29baa2d81ebf2fd19a25b8408a2f77a8ac71238e
Lucasvgb <lucasvgb@id.uff.br>
2023-09-12 22:45:58 -0300

    ,

---
+++ vetores/nota4AlunosVet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+  float nota1,nota2,nota3,nota4;
+
+  printf("Digite a nota do Primeiro aluno: ");
+  scanf("%f", &nota1);
+
+  printf("Digite a nota do Primeiro aluno: ");
+  scanf("%f", &nota2);
+
+  printf("Digite a nota do Primeiro aluno: ");
+  scanf("%f", &nota3);
+
+  printf("Digite a nota do Primeiro aluno: ");
+  scanf("%f", &nota4);
+
+
+  return 0;
+}
+
