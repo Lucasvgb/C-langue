844e9e085146edd0d6fa6c26da6b5a8b8c4e24ae
Lucasvgb <lucasvgb@id.uff.br>
2023-10-25 15:18:44 -0300

    .

---
+++ exerciciosLivros/numerosCrescente.c
@@ -0,0 +1,16 @@
+// Capítulo 07 - Decisão - Imprimir dois números em ordem crescente. pg 95
+int main(){
+
+  int a,b;
+  scanf("%d%d", &a, &b);
+  
+  if(a > b){
+    printf("%d eh maior do que %d",a,b);
+  }
+  else if(a < b){
+
+  }
+
+
+  return 0;
+}
